37a3b32c8f95dcef25db37ea0e5235b0eeea1d8e
c0ahja01 <C0ahja01@students.oamk.fi>
2022-02-17 22:03:37 +0200

    uusinta

---
+++ Exercise1/src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function  () {
+  return <div>
+      <h1 className="bigTitle">PÄIVÄN TIMANTTI: Tutkija kertoo: Näin sota näkyy meissä edelleen</h1>
+      <h1 className="bigTitle">PÄIVÄN TIMANTTI: Harriet Rabb tajusi avioliiton taloudelliset hyödyt ja kosi-Näin hän säästäisiaviopuolisona 19200 euroa</h1>
+  </div>;
+}
