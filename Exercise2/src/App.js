1b5c3ae0248d405dc09b5b64ca2ddb441a6b3782
c0ahja01 <90405130+c0ahja01@users.noreply.github.com>
2022-02-28 22:56:55 +0200

    Add files via upload

--- Exercise2/src/App.js
+++ Exercise2/src/App.js
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from './components/Title';
 import ShoppingList from './components/ShoppingList';
+import ShoppingListItem from './components/ShoppingListItem';
 import styles from './App.module.css';
 import './App.css';
 
-/* A ES6 class style stateful component for the shopping list application */
-class App extends React.Component {
-  constructor(props)
-  {
-    /* You should call super(props) before any other statement. 
-       Otherwise, this.props will be undefined in the constructor, which can lead to bugs.
-    */
-    super(props);
+export default function App(props) {
 
-    this.state = {
-      items: [
-        { id: 1, value: 'Milk', qty: 5, unit: 'ltr' },
-        { id: 2, value: 'Bananas', qty: 6, unit: 'pcs' },
-        { id: 3, value: 'Bread', qty: 3, unit: 'x' },
-        { id: 4, value: 'Eggs', qty: 16, unit: 'x' }
-      ]
-    };
+  const[ counterValue, setCounterValue ] = useState(0);
+  //const[ shoppingListItem, setShoppingListItem ] = useState([
+
+  const [items, setItems] = useState([
+    { id: 1, value: 'Milk', qty: 5, unit: 'ltr', isChecked: true},
+    { id: 2, value: 'Bananas', qty: 6, unit: 'pcs', isChecked: false},
+    { id: 3, value: 'Bread', qty: 3, unit: 'x', isChecked: true},
+    { id: 4, value: 'Eggs', qty: 16, unit: 'x', isChecked: false},
+  ]);
+  //]);
+  const { applicationDescription, applicationName } = props;
+
+  const handleItemCheckedToggle = (items) => {
+    console.log("Toggling item status");
+    console.log(items);
+
+    let newitems = [...items];
+
+    let itemClickedIndex = newitems.findIndex(i => items.id == i.id);
+    if(itemClickedIndex != -1) {
+      //itemClicked.isChecked = ! itemClicked.isChecked;
+
+      let newElement = {...newitems[itemClickedIndex]}
+      newElement.qty = newElement.qty;
+      newitems[itemClickedIndex] = newElement;
+      setItems(newitems);
+    }
 
   }
-
-  render()
-  {
-    const { applicationDescription, applicationName } = this.props;
-    return <div className={ styles.shoppingList }>
-      <Title 
-        applicationDescription={ applicationDescription }
-        applicationName={ applicationName }
-      />
-      <ShoppingList items={ this.state.items } />
-    </div>
-  }
-}
-
-export default App;
-
-
-
-
+  return <div className={ styles.shoppingList }>
+    <Title 
+      applicationDescription={ applicationDescription }
+      applicationName={ applicationName }
+    />
+    <div>Laskuri: { counterValue }</div>
+    <button onClick = { () => setCounterValue(counterValue + 1)}> Increase counter!</button>
+    <ShoppingList items={ items } />
+    
+    <button onClick = { () => setItems(items)}>Add Milk</button>
+    <button>Add Bananas</button>
+    <button>Add Bread</button>
+    <button>Add Eggs</button> 
+  </div>
+} 
